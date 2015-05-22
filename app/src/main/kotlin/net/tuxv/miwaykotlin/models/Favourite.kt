f37742ddde6886eb64a6b2db8829dcbf60079402
Yasith Vidanaarachchi <yasith@tuxv.net>
2015-05-22 02:41:36 -0400

    Remove some comments and logging

--- app/src/main/kotlin/net/tuxv/miwaykotlin/models/Favourite.kt
+++ app/src/main/kotlin/net/tuxv/miwaykotlin/models/Favourite.kt
@@ -1,9 +1,5 @@
 package net.tuxv.miwaykotlin.models
 
-/**
- * Created by yasith on 15-05-21.
- */
-
 data class Favourite {
     // TODO
 }
--- app/src/main/kotlin/net/tuxv/miwaykotlin/models/Route.kt
+++ app/src/main/kotlin/net/tuxv/miwaykotlin/models/Route.kt
@@ -2,10 +2,6 @@ package net.tuxv.miwaykotlin.models
 
 import com.google.gson.annotations.Expose
 
-/**
- * Created by yasith on 15-05-20.
- */
-
 class Route {
     var id : String? = null
     var name : String? = null
