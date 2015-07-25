6037d46de668c81d54bcad3ec12dc27fa1e85f62
Yasith Vidanaarachchi <yasith@tuxv.net>
2015-07-25 17:54:46 -0400

    Dynamic next stop times

--- app/src/main/kotlin/net/tuxv/miwaykotlin/models/Time.kt
+++ app/src/main/kotlin/net/tuxv/miwaykotlin/models/Time.kt
@@ -1,19 +1,27 @@
 package net.tuxv.miwaykotlin.models
 
-import java.util.LinkedList
+import java.util.*
+import java.lang.String;
 
-public class Time(val hour : Int, val minute : Int, val ampm : String) : Comparable<Time> {
 
-    public var notes : LinkedList<String> = LinkedList()
+public class Time: Comparable<Time> {
+
+    var hour : Int? = null
+    var minute : Int? = null
 
     // TODO: Count ampm
     override fun compareTo(other: Time) = when{
-        this.hour < other.hour -> -1
-        this.hour == other.hour && this.minute < other.minute -> -1
+        this.hour!! < other.hour!! -> -1
+        this.hour!! == other.hour!! && this.minute!! < other.minute!! -> -1
         else -> 1
     }
 
-    override fun toString() = "${hour}:${minute}:${ampm}"
+    override fun toString() = "${hour}:${String.format("%02d", minute)}"
 
     // TODO: 24 hour time
+
+    class TimeResponse {
+        var data : List<Time> = ArrayList()
+    }
+
 }
